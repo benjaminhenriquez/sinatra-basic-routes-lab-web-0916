33aed706e2f51ed0c47b2aa4740b900d4f013a2d
Ben <henriquez.ben@gmail.com>
2016-10-13 13:40:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Ben"
+  end
+  get '/hometown' do
+    "My hometown is San Francisco"
+  end
+  get '/favorite-song' do
+    "My favorite song is Helplessness Blues by Fleet Foxes"
+  end
 end
